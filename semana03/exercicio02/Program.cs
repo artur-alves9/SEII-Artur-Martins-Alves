1967a1429ebd0f82bf496259ce8fb18511d6cd71
Artur <artur_alves9@hotmail.com>
2024-08-16 16:51:39 -0300

    asd

---
+++ semana03/exercicio02/Program.cs
@@ -0,0 +1,32 @@
+﻿string [] myarray = new string[5];
+myarray[0] = "seg";
+myarray[1] = "ter";
+myarray[2] = "quart";
+myarray[3] = "quinta";
+myarray[4] = "sexta";
+for(int i=0;i<5;i++){
+Console.WriteLine(myarray[i]);
+
+}
+List <string> list = new List<string>();
+list.Add("seg");
+list.Add("ter");
+list.Add("qua");
+list.Add("qui");
+list.Add("sex");
+for(int i=0;i<list.Count;i++){
+list[i]=list[i].Substring(0,2);
+Console.WriteLine(list[i]);
+
+}
+Dictionary<int,string> dictionary = new Dictionary<int,string>();
+dictionary[0] = "seg";
+dictionary[1] = "ter";
+dictionary[2] = "quart";
+dictionary[3] = "quinta";
+dictionary[4] = "sexta";
+
+for(int i=0;i<dictionary.Count;i++){
+Console.WriteLine(myarray[i]);
+
+}
---
+++ semana03/exercicio03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CScurso {
+class Program
+{
+    public static void Main(string[] args)
+    {   string nome_arquivo = "arquivo.txt";
+        string texto = "meutexto";
+        File.WriteAllText(nome_arquivo,texto);
+    Console.WriteLine("arquivo criado!");
+        
+        
+       string[] documentos = {"linha2","linha3"};
+        
+        File.WriteAllLines(nome_arquivo,documentos);
+        string[] texto1 = File.ReadAllLines(nome_arquivo);
+        foreach(string linha in texto1);
+        Console.WriteLine(texto1);
+        Console.ReadKey(true);
+    }
+
+}
+
+
+}
